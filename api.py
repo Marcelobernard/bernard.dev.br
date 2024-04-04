61de7044354e336f471c983233a3dceb8d0a4a69
Marcelobernard <59776037+Marcelobernard@users.noreply.github.com>
2024-04-04 13:45:57 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,16 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Rota para receber comandos dos bots
 @app.route('/', methods=['POST'])
 def receber_comandos():
-    dados = request.json  # Recebe os dados JSON enviados pelo bot
+    dados = request.json
     print("Comando recebido:", dados['comando'])
     
-    # Executa o comando e retorna um resultado (exemplo)
     resultado = "Comando executado com sucesso"
     
-    # Retorna o resultado para o bot
     return jsonify({'resultado': resultado})
 
 if __name__ == '__main__':
