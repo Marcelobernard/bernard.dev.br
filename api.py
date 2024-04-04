6a750c4a4d495adf27e1886a7b8c8f91c1531a81
Marcelobernard <59776037+Marcelobernard@users.noreply.github.com>
2024-04-04 13:53:10 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+comandos_para_cliente = ['ls', 'pwd', 'whoami']
+
 @app.route('/', methods=['POST'])
 def receber_comandos():
     dados = request.json
@@ -9,7 +11,11 @@ def receber_comandos():
     
     resultado = "Comando executado com sucesso"
     
-    return jsonify({'resultado': resultado})
+    if comandos_para_cliente:
+        comando_para_enviar = comandos_para_cliente.pop(0)
+        return jsonify({'comandos': [comando_para_enviar]})
+    else:
+        return jsonify({'comandos': []})
 
 if __name__ == '__main__':
     app.run(debug=True)
