8317e39af523c217204d0f49a7aa5b551b7d7bb2
Marcelo Bernard <m4rc3l0.64@gmail.com>
2024-09-04 09:18:29 -0300

    Criação do arquivo de servidor

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'API funcionando!' });
+});
+
+app.get('/api/*', (req, res) => {
+  res.status(404).send('Rota não encontrada');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+app.post('/api/order', (req, res) => {
+    const orderData = req.body;
+    console.log('Pedido recebido:', orderData);
+    res.status(200).json({ message: 'Pedido recebido com sucesso!' });
+  });
