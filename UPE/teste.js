4aede09075190a5146ce60c3c07c98bf6feddd9f
Marcelo Bernard <m4rc3l0.64@gmail.com>
2025-08-27 17:53:35 -0300

    Mobile e Full

---
+++ UPE/teste.js
@@ -0,0 +1,18 @@
+let colors = [ 'blue', 'red', 'green', 'yellow', 'purple', 'orange']
+let indexSlider = 0;
+let index = 0;
+
+const slider = () => {
+    imgSlider.style.transform = 'rotate($(indexSlider * 60)deg)';
+    items.forEach(item => {
+        item.style.transform = 'rotate($(index * -60)deg)';
+});
+
+document.querySelector('.img-item.active').classList.remove('active');
+imgItems[index].classList.add('active');
+
+document.querySelector('info-item.active').classList.remove('active');
+infoItems[index].classList.add('active');
+
+document.body.style.background = colors[index];
+}
